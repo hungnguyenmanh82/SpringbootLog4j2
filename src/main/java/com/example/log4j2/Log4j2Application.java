16491bf3eb55403c367a14d92b3ba1fb46f8ed89
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2019-04-20 23:16:06 +0700

    update

--- src/main/java/com/example/log4j2/Log4j2Application.java
+++ src/main/java/com/example/log4j2/Log4j2Application.java
@@ -2,10 +2,13 @@ package com.example.log4j2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+
 //để tăng performance lúc start Springboot
 //@ComponentScan({"com.in28minutes.springboot.basics.springbootin10steps","com.in28minutes.springboot.somethingelse"})
+@SpringBootApplication
+@ComponentScan({"com.example.log4j2.*"})
 public class Log4j2Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/log4j2/controller/Log4j2Controller.java
+++ src/main/java/com/example/log4j2/controller/Log4j2Controller.java
@@ -20,17 +20,43 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class Log4j2Controller {
 	
-	private static final Logger log = LogManager.getLogger(Log4j2Controller.class);
+	/**
+	 * "console": là tên appenders
+	 * neu ko tim thấy tên thì nó sẽ gan cho Root appender
+	 */
+	private static final Logger log = LogManager.getLogger(Log4j2Controller.class); // %c = package name
+	
+	//neu ko tim thay ten "logger1" ở log4j2.xml => thì log nay sẽ ghi ra "root" logger
+	private static final Logger fileLogger1 = LogManager.getLogger("logger1"); // c% =c%{1} = c%{2}= "logger1"
+	
+	//neu ko tim thay ten "logger2" ở log4j2.xml => thì log nay sẽ ghi ra "root" logger
+	private static final Logger fileLogger2 = LogManager.getLogger("logger2"); // c% =c%{1} = c%{2}= "logger2"
 	
 	@RequestMapping(path = { "/", "/index" }, method = RequestMethod.GET)
 	public String home(Model model){
 		//thứ tự ưu tiên từ cao xuống thấp
-		log.trace("/index"); //code này ko hiển thị vì log level ở file Log4j2.xml
-		log.debug("/index");
-		log.info("/index");
-		log.warn("/index");
-		log.error("/index");
-		log.fatal("/index");
+		log.trace("/index trace"); //code này ko hiển thị vì log level ở file Log4j2.xml
+		log.debug("/index debug");
+		log.info("/index info");
+		log.warn("/index warning");
+		log.error("/index error");
+		log.fatal("/index fatal");
+		
+		//
+		fileLogger1.trace("/index trace"); //code này ko hiển thị vì log level ở file Log4j2.xml
+		fileLogger1.debug("/index debug");
+		fileLogger1.info("/index info");
+		fileLogger1.warn("/index warning");
+		fileLogger1.error("/index error");
+		fileLogger1.fatal("/index fatal");
+		
+		//
+		fileLogger2.trace("/index trace"); //code này ko hiển thị vì log level ở file Log4j2.xml
+		fileLogger2.debug("/index debug");
+		fileLogger2.info("/index info");
+		fileLogger2.warn("/index warning");
+		fileLogger2.error("/index error");
+		fileLogger2.fatal("/index fatal");
 
 		return "index";
 	}
